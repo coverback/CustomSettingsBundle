50e98609b3b48d458450832a7f20ca7bfa1b2b6d
Romans Karpelcevs <karpelcev@gmail.com>
2017-08-05 17:43:44 +0300

    Initial Commit

---
+++ CustomSettingsBundle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSettingsBundle
+//
+//  Created by Romans Karpelcevs on 05/08/2017.
+//  Copyright © 2017 Romans Karpelcevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
